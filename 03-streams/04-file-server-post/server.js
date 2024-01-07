0593de811e1e8db5a91655ea4bc27e61055c1cd2
Nikita Sazonov <almonah@gmail.com>
2024-01-08 02:42:44 +0300

    solution fixes

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -10,65 +10,62 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
-  const parsedPathname = path.parse(pathname);
-
-  if (parsedPathname.dir) {
-    res.statusCode = 400;
-    res.end('Incorrect path');
-    return;
-  }
-
   const filepath = path.join(__dirname, 'files', pathname);
-
-  try {
-    if (fs.existsSync(filepath)) {
-      res.statusCode = 409;
-      res.end('File exists');
-      return;
-    }
-  } catch (err) {
-    res.statusCode = 500;
-    res.end('Server error');
-    return;
-  }
-
-  const limitSizeStream = new LimitSizeStream({limit: 1048576});
-  const writableStream = fs.createWriteStream(filepath);
-
-  writableStream.on('finish', () => {
-    console.log(`writableStream finish`);
-    res.statusCode = 201;
-    res.end('File saved succesfully');
-  });
-
-  req.on('error', () => {
-    console.log(`Connection aborted`);
-    fs.unlink(filepath, () => {
-      console.log(`File ${filepath} removed`);
-    });
-  });
-
-  limitSizeStream.on('error', (error) => {
-    switch (error.code) {
-      case 'LIMIT_EXCEEDED': {
-        console.log(`File ${filepath} is too large`);
-        res.statusCode = 413;
-        res.end('File too large');
-        writableStream.close();
-        fs.unlink(filepath, () => {
-          console.log(`File ${filepath} removed`);
-        });
-        break;
-      }
-      default: {
-        res.statusCode = 500;
-        res.end('Server error');
-      }
-    }
-  });
 
   switch (req.method) {
     case 'POST':
+      const parsedPathname = path.parse(pathname);
+
+      if (parsedPathname.dir) {
+        res.statusCode = 400;
+        res.end('Incorrect path');
+        return;
+      }
+
+      const limitSizeStream = new LimitSizeStream({limit: 1048576});
+      const writableStream = fs.createWriteStream(filepath, {flags: 'wx'});
+
+      writableStream.on('finish', () => {
+        console.log(`writableStream finish`);
+        res.statusCode = 201;
+        res.end('File saved succesfully');
+      });
+
+      writableStream.on('error', (error) => {
+        if (error.code === 'EEXIST') {
+          res.statusCode = 409;
+          res.end('File already exist');
+        } else {
+          res.statusCode = 500;
+          res.end('Server error');
+        }
+      });
+
+      res.on('close', () => {
+        if (!req.complete) {
+          fs.unlink(filepath, () => {
+            console.log(`File ${filepath} removed`);
+          });
+        }
+      });
+
+      limitSizeStream.on('error', (error) => {
+        switch (error.code) {
+          case 'LIMIT_EXCEEDED': {
+            console.log(`File ${filepath} is too large`);
+            res.statusCode = 413;
+            res.end('File too large');
+            // writableStream.close();
+            break;
+          }
+          default: {
+            res.statusCode = 500;
+            res.end('Server error');
+          }
+        }
+      });
+
+
       req.pipe(limitSizeStream).pipe(writableStream);
       break;
 
