a19e0608834281344cb25064a615091a521f0c52
Nikita Sazonov <almonah@gmail.com>
2024-01-19 11:56:05 +0300

    solutions for: 06-mongodb-mongoose/01-schema-model 06-mongodb-mongoose/02-rest-api

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,35 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+const productMapper = require('../mappers/product');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
-
   if (!subcategory) return next();
-
-  ctx.body = {};
+  const isValid = mongoose.Types.ObjectId.isValid(subcategory);
+  if (!isValid) return next();
+  const products = await Product.find({subcategory: subcategory});
+  ctx.body = {products: products.map(productMapper)};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find();
+  ctx.body = {products: products.map(productMapper)};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const id = ctx.params.id;
+  const isValid = mongoose.Types.ObjectId.isValid(id);
+  if (!isValid) {
+    ctx.status = 400;
+    ctx.body = {};
+    return;
+  }
+  const product = await Product.findOne({_id: id});
+  if (!product) {
+    ctx.status = 404;
+    ctx.body = {};
+    return;
+  }
+  ctx.body = {product: productMapper(product)};
 };
 
